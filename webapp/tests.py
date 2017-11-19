0e0a804a4246a2c1c1f48d8d0e398e3d07a13471
Bryan Chan <xchangip@gmail.com>
2017-11-19 15:21:53 -0600

    Testing tests

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from .models import Student, StudentForm, Review, ReviewForm, Course, CourseForm, Professor, ProfessorForm, ReviewLike
 
 # Create your tests here.
+
+class StudentTestCase(TestCase):
+	def setUp(self):
+		Student.objects.create(
+			username="test",
+			firstname="test",
+			lastname="test",
+			email="test",
+			career="test",
+			interest1="test",
+			interest2="test",
+			interest3="test",
+			highschool="test"
+		)
+
+		Student.objects.create(
+			username="test2",
+			firstname="test2",
+			lastname="test2",
+			email="test2",
+			career="test2",
+			interest1="test2",
+			interest2="test2",
+			interest3="test2",
+			highschool="test2"
+		)
+
+	def testStudents(self):
+		test = Student.objects.get(username="test")
+		anotherTest = Student.objects.get(username="test2")
+
+		self.assertEqual(test.username, 'test')

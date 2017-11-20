406acfaffe922c1b228dd19b83ec8589e5aa45b6
jorge97710 <jazmitia_10@hotmail.com>
2017-11-20 00:23:24 -0600

    cambios

--- webapp/Deteccion de Validez modulo.py
+++ webapp/Deteccion de Validez modulo.py
@@ -1,46 +1,48 @@
 # -*- coding: utf-8 -*-
 # universidad del Valle de Guatemala
+from __future__ import division
 
 
 def detection(x):
-    malisimas = ['estupida', 'estupido', 'estúpido', 'idiota','pendejo', 'tarado', 'imbecil', 'puta', 'mierda', 'cerote', 'cerota', 'puto']
-    malas = ['poco','impuntual','impaciente','desatento','injusto','perdida','poco','inflexible','inutil','desconsiderado', 'inútil','incompetente','tonto', 'tonta',  'flojo', 'ineficiente', 'inepto', 'incapaz', 'pesado', 'abusivo', 'racista', 'machista', 'incomodo']
-    buenas = ['mucho','paciente','justo','util','útil','bueno','atento','puntual','considerado','buena', 'bien', 'excelente', 'interesante', 'esfuerzo', 'esfuerza', 'gusta', 'gusto', 'cómodo', 'comodo']
+    malisimas = ['estupida', 'estupido', 'estúpido', 'idiota', 'pendejo', 'tarado', 'imbecil', 'puta', 'mierda',
+                 'cerote', 'cerota', 'puto']
+    malas = ['poco', 'impuntual', 'impaciente', 'desatento', 'injusto', 'perdida', 'poco', 'inflexible', 'inutil',
+             'desconsiderado', 'inútil', 'incompetente', 'tonto', 'tonta', 'flojo', 'ineficiente', 'inepto', 'incapaz',
+             'pesado', 'abusivo', 'racista', 'machista', 'incomodo','mal','malo','mala']
+    buenas = ['mucho', 'paciente', 'justo', 'util', 'útil', 'bueno', 'atento', 'puntual', 'considerado', 'buena',
+              'bien', 'excelente', 'interesante', 'esfuerzo', 'esfuerza', 'gusta', 'gusto', 'cómodo', 'comodo', 'recomendado','recomendada',]
     calificacion = 0
-    cont_malas=0
-    cont_buenas=0
+    cont_malas = 0
+    cont_buenas = 0
 
-    clasificacion_prueba = 0.43
+    clasificacion_prueba = 0.0
 
     x = x.strip()
 
-    cantidad_palabras = len ( x.split(" "))
+    cantidad_palabras = len(x.split(" "))
 
-    #print "el tamaño del texto es "+ cantidad_palabras
-
-    print "esto es"
-    print x.split(" ")
-
-    for i in range (len (x.split(" "))):
-        #censuramos el comentario
-        if i in malisimas:
-            x.replace(i,"#@#@!")
-        if i in malas:
+    for i in range(len(x.split(" "))):
+        # censuramos el comentario
+        if x.split(" ")[i] in malisimas:
+            print "entre"
             cont_malas = cont_malas + 1
-        if i in buenas:
+        if x.split(" ")[i] in malas:
+            print "entre"
+            cont_malas = cont_malas + 1
+        if x.split(" ")[i] in buenas:
+            print "entre"
             cont_buenas = cont_buenas + 1
-    #si clasificacion_prueba es negativo el comentario es negativo y se denotará que tan negativo
-    if cont_malas>cont_buenas:
-        clasificaion_prueba = (cont_malas / cantidad_palabras)*-1
-    #si clasificacion_prueba es positiva el comentario es positiva y se denotará que tan positiva
-    if cont_malas<cont_buenas:
-        clasificaion_prueba = cont_buenas / cantidad_palabras
-            
+    # si clasificacion_prueba es negativo el comentario es negativo y se denotará que tan negativo
+    if cont_malas > cont_buenas:
+        clasificaion_prueba = float(cont_malas / cantidad_palabras) * (-1)
+        return float(cont_malas / cantidad_palabras) * (-1) * 10
+    # si clasificacion_prueba es positiva el comentario es positiva y se denotará que tan positiva
+    if cont_malas < cont_buenas:
+        clasificaion_prueba = float(cont_buenas / cantidad_palabras)
+        return float(cont_buenas / cantidad_palabras) * 10
+
     ##-----------------calificacion = malas o buenas / cantidad_palabras-----------------------
 
-    calificacion = clasificacion_prueba * 10
-    print calificacion
-    return calificacion
 
 texto = raw_input("Ingrese el texto a revisar: ")
-detection(texto)
+print "la clasificacion es", detection(texto)

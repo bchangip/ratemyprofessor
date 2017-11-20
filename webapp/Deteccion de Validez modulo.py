59489785f7745e96cfe1c92b400ef01b5ff32d39
jenbarillas <bar15307@uvg.edu.gt>
2017-11-19 23:57:24 -0600

    Update Deteccion de Validez modulo.py

--- webapp/Deteccion de Validez modulo.py
+++ webapp/Deteccion de Validez modulo.py
@@ -3,33 +3,44 @@
 
 
 def detection(x):
-
+    malisimas = ['estupida', 'estupido', 'estúpido', 'idiota','pendejo', 'tarado', 'imbecil', 'puta', 'mierda', 'cerote', 'cerota', 'puto']
+    malas = ['poco','impuntual','impaciente','desatento','injusto','perdida','poco','inflexible','inutil','desconsiderado', 'inútil','incompetente','tonto', 'tonta',  'flojo', 'ineficiente', 'inepto', 'incapaz', 'pesado', 'abusivo', 'racista', 'machista', 'incomodo']
+    buenas = ['mucho','paciente','justo','util','útil','bueno','atento','puntual','considerado','buena', 'bien', 'excelente', 'interesante', 'esfuerzo', 'esfuerza', 'gusta', 'gusto', 'cómodo', 'comodo']
     calificacion = 0
-    malas=0
+    cont_malas=0
+    cont_buenas=0
 
     clasificacion_prueba = 0.43
 
     x = x.strip()
 
-    cantidad_palabras = str(len ( x.split(" ")) )
+    cantidad_palabras = len ( x.split(" "))
 
-    print "el tamaño del texto es", cantidad_palabras
+    #print "el tamaño del texto es "+ cantidad_palabras
 
     print "esto es"
     print x.split(" ")
 
     for i in range (len (x.split(" "))):
-        print "entre"
-
-        #aqui tienes que hacer lo de la revision
-
-
-    ##-----------------calificacion = malas / cantidad_palabras-----------------------
+        #censuramos el comentario
+        if i in malisimas:
+            x.replace(i,"#@#@!")
+        if i in malas:
+            cont_malas = cont_malas + 1
+        if i in buenas:
+            cont_buenas = cont_buenas + 1
+    #si clasificacion_prueba es negativo el comentario es negativo y se denotará que tan negativo
+    if cont_malas>cont_buenas:
+        clasificaion_prueba = (cont_malas / cantidad_palabras)*-1
+    #si clasificacion_prueba es positiva el comentario es positiva y se denotará que tan positiva
+    if cont_malas<cont_buenas:
+        clasificaion_prueba = cont_buenas / cantidad_palabras
+            
+    ##-----------------calificacion = malas o buenas / cantidad_palabras-----------------------
 
     calificacion = clasificacion_prueba * 10
-
+    print calificacion
     return calificacion
-
 
 texto = raw_input("Ingrese el texto a revisar: ")
 detection(texto)

056f3feb3e48e8fae0c28b16b9a238a71ed48da8
vagrant <xchangip@gmail.com>
2017-11-20 20:37:52 +0000

    CI fixes

--- ratemyprofessor/settings.py
+++ ratemyprofessor/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import dj_database_url
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,22 +81,33 @@ WSGI_APPLICATION = 'ratemyprofessor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+
+import sys
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ratemyprofessor',
+        'USER': 'runner',
+        'PASSWORD': 'semaphoredb',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ratemyprofessor',
-        'USER': 'runner',
-        'PASSWORD': 'semaphoredb',
-        # 'USER': 'bchangip',
-        # 'PASSWORD': 'bchangip',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-    # 'default': dj_database_url.config()
+    # 'test': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'ratemyprofessor',
+    #     'USER': 'runner',
+    #     'PASSWORD': 'semaphoredb',
+    #     # 'USER': 'bchangip',
+    #     # 'PASSWORD': 'bchangip',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
+    'default': dj_database_url.config()
 }
 
 # Password validation
